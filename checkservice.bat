07f7200995be3f04674aba55183ddeb08bd191d7
Andrei <andrei@aimcorp.co.uk>
2021-08-03 12:27:22 +0200

    Added .bat pusher script for easy deployment.

---
+++ checkservice.bat
@@ -0,0 +1,2 @@
+%windir%\sysnative\cmd.exe /c powershell.exe ExecutionPolicy -bypass -file .\serviceevent.ps1
+%windir%\sysnative\cmd.exe /c powershell.exe ExecutionPolicy -bypass -file .\serviceevent.ps1
